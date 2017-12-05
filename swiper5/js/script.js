5249e43a7e76f85d78e9503ab04f60f62f82e76e
范大米 <2494728648@qq.com>
2017-12-05 10:11:16 +0800

    微场景

---
+++ swiper5/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lenovo on 17/12/5 005.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // 如果需要滚动条
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
