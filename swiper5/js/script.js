4ebf98dc2e61ed128af90e0e2971afc51aab1dd9
范大米 <2494728648@qq.com>
2017-12-08 09:16:50 +0800

    第二屏OK

--- swiper5/js/script.js
+++ swiper5/js/script.js
@@ -16,4 +16,13 @@ var swiper = new Swiper('.swiper-container', {
     scrollbar: {
         el: '.swiper-scrollbar',
     },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
